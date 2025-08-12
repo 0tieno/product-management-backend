08db1453bfd0bef27960ee6d421bd01b539a0818
0tieno <ronvi035@gmail.com>
2025-08-12 13:35:39 +0300

    feat: add routing with react-router-dom, create HomePage and CreatePage components, and implement Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import { Button } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
-    <Button colorScheme='teal'>Hello</Button>
+    <Box minH="100vh" py="12" px={{ base: "4", md: "8" }}>
+      
+      {/* navbar */}
+      <Navbar />
+      
+      {/* main content */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   )
 }
 
